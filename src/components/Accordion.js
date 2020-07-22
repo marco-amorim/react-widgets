3d5e6d5c147462e14bb785c0ea9b9cc77dbd83b6
marco-amorim <marcoamorim95@hotmail.com>
2020-07-22 20:29:31 -0300

    :rocket: manage open item based on state

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-	const [activeIndex, setActiveIndex] = useState(null);
+	const [activeIndex, setActiveIndex] = useState(0);
 
 	const onTitleClick = (index) => {
 		setActiveIndex(index);
@@ -10,18 +10,18 @@ const Accordion = ({ items }) => {
 	const renderedItems = items.map((item, index) => {
 		return (
 			<React.Fragment key={item.title}>
-				<div onClick={() => onTitleClick(index)} className="title active">
+				<div onClick={() => onTitleClick(index)} className={`title ${activeIndex === index ? "active" : ""}`}>
 					<i className="dropdown icon"></i>
 					{item.title}
 				</div>
-				<div className="content active">
+				<div className={`content ${activeIndex === index ? "active" : ""}`} >
 					<p>{item.content}</p>
 				</div>
 			</React.Fragment>
 		);
 	});
 
-return <div className="ui styled accordion">{renderedItems} <h1>{activeIndex}</h1></div>;
+return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
