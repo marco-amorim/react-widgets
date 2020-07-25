7087f5a5850c1d59ef61f9c2ec64740e64da937c
marco-amorim <marcoamorim95@hotmail.com>
2020-07-25 00:50:20 -0300

    :rocket: search widget with useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,59 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-	const [term, setTerm] = useState('');
+	const [term, setTerm] = useState('programming');
+	const [results, setResults] = useState([]);
 
 	useEffect(() => {
-		console.log(term);
+		const search = async () => {
+			const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+				params: {
+					action: 'query',
+					list: 'search',
+					origin: '*',
+					format: 'json',
+					srsearch: term,
+				},
+			});
+			setResults(data.query.search);
+		};
+
+		if (term && !results.length) {
+			search();
+		} else {
+			const timeoutId = setTimeout(() => {
+				if (term) {
+					search();
+				}
+			}, 1000);
+
+			return () => {
+				clearTimeout(timeoutId);
+			};
+		}
 	}, [term]);
+
+	const renderedResults = results.map((result) => {
+		return (
+			<div className="item" key={result.pageid}>
+				<div className="right floated content">
+					<a
+						className="ui button"
+						href={`https://en.wikipedia.org?curid=${result.pageid}`}
+					>
+						Go
+					</a>
+				</div>
+				<div className="content">
+					<div className="header">{result.title}</div>
+					{/* dangerouslySetInnerHTML is a "secret" feature in React that can make your
+					 app unsafe because it renders HTML from your requests directly into your app! */}
+					<span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+				</div>
+			</div>
+		);
+	});
 
 	return (
 		<div>
@@ -19,6 +67,7 @@ const Search = () => {
 					/>
 				</div>
 			</div>
+			<div className="ui celled list">{renderedResults}</div>
 		</div>
 	);
 };
