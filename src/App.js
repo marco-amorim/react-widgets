bdd249ce292419503290ef77ffef68548a21566f
marco-amorim <marcoamorim95@hotmail.com>
2020-07-26 23:47:20 -0300

    :rocket: passing a prop that is a state from App to Dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // eslint-disable-next-line
 import Accordion from './components/Accordion';
 // eslint-disable-next-line
@@ -38,11 +38,17 @@ const options = [
 ];
 
 export default () => {
+	const [selected, setSelected] = useState(options[0]);
+
 	return (
 		<div>
 			{/* <Accordion items={items}></Accordion> */}
 			{/* <Search></Search> */}
-			<Dropdown options={options} />
+			<Dropdown
+				selected={selected}
+				options={options}
+				onSelectedChange={setSelected}
+			/>
 		</div>
 	);
 };
